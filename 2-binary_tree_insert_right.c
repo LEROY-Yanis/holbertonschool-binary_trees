5e57de6a03a04a842bc0389726310943e3fbe590
LEROY-Yanis <11479@holbertonstudents.com>
2025-08-05 10:47:13 +0200

    fix the parent = null

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new = binary_tree_node(parent, value);
 
 	if (parent->right != NULL)
